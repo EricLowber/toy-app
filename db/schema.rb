32c4f8057041a3de90853e0c1cea07823ea81ccf
Eric Lowber <elowber@gmail.com>
2015-05-05 12:34:10 -0500

    added image

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,4 @@ ActiveRecord::Schema.define(version: 20150430144211) do
     t.datetime "updated_at",         null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
